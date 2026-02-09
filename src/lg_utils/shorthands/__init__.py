3abad49db023835758be5b5dc72fe6b51328798e
kuanpern <kuanpern@sph.com.sg>
2026-02-09 12:18:04 +0800

    add init_chat_model shorthand

---
+++ src/lg_utils/shorthands/__init__.py
@@ -0,0 +1,29 @@
+from langchain.chat_models import init_chat_model
+
+class ChatModelManager:
+    def __init__(self, configs):
+        self._chat_models = {}
+        self._configs = configs
+        self._supported_models = list(configs.keys())
+
+    def init_chat_model(self, model_name: str, **kwargs):
+        """
+        Initializes and caches LangChain chat models.
+
+        Args:
+            model_name (str): The name of the model to retrieve.
+            **kwargs: Additional keyword arguments to pass to init_chat_model.
+
+        Returns:
+            langchain.chat_models.base.BaseChatModel: An initialized chat model.
+        """
+        chat_model = self._chat_models.get(model_name)
+        if chat_model is not None:
+            return chat_model
+
+        assert model_name in self._supported_models, f"{model_name} is not supported."
+        default_kwargs = self._configs.get(model_name)
+        combined_kwargs = {"model": "default", **default_kwargs, **kwargs}
+        chat_model = init_chat_model(**combined_kwargs)
+        self._chat_models[model_name] = chat_model
+        return chat_model
